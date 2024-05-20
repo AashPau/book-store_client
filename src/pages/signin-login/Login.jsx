7da0d3450dac904951109469e9cbc0cd7f045625
Aash <31497659+AashPau@users.noreply.github.com>
2024-05-20 13:14:36 +1000

    add login page

--- src/pages/signin-login/Login.jsx
+++ src/pages/signin-login/Login.jsx
@@ -1,7 +1,66 @@
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { DefaultLayout } from "../../components/layout/DefaultLayout";
+import { CustomInput } from "../../components/customInput/CustomInput";
+import { useRef } from "react";
+import { toast } from "react-toastify";
 
-const Login = () => {
-  return <DefaultLayout>loginIn</DefaultLayout>;
+const SignIn = () => {
+  const emailRef = useRef("");
+  const passRef = useRef("");
+
+  const inputs = [
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      required: true,
+      placeholder: "lala@lala.lala",
+      inputRef: emailRef,
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+      required: true,
+      placeholder: "******",
+      inputRef: passRef,
+    },
+  ];
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passRef.current.value;
+    console.log(email, password);
+    if (!email || !password) {
+      return toast.error("Botn field must be filled");
+    }
+  };
+  return (
+    <DefaultLayout>
+      <Row>
+        <Col>
+          <Form
+            onSubmit={handleOnSubmit}
+            className="shadow-lg border p-5 rounded  m-auto mt-4"
+            style={{ width: "450px" }}
+          >
+            <h1>Welcome back!</h1>
+            <hr />
+            {inputs.map((input, i) => (
+              <CustomInput key={i} {...input} />
+            ))}
+
+            <div className="d-grid">
+              <Button type="submit" className="btn-primary">
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </DefaultLayout>
+  );
 };
 
-export default Login;
+export default SignIn;
